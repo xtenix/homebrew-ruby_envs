078b356c0be0909d6f0fcb1413d6f0faba5beb6e
Dean Hallman <dhallman@realgravity.com>
2013-05-16 10:38:48 -0400

    testing

--- build-devbox.rb
+++ build-devbox.rb
@@ -9,7 +9,7 @@ class BuildDevbox < Formula
 
   head 'https://github.com/dhallman/homebrew-ruby_envs.git', :branch => 'master'
 
-  add_tap 'phinze/cask'
+  #add_tap 'phinze/cask'
   depends_on 'chruby'
   depends_on 'ruby-build'
   #depends_on 'brew-cask'
