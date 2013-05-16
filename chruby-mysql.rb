09f0c9e42ac28c776bee7d15077f2bb93508fb24
Dean Hallman <dhallman@realgravity.com>
2013-05-15 22:55:40 -0400

    Trying to fix head only warning

--- chruby-mysql.rb
+++ chruby-mysql.rb
@@ -3,6 +3,7 @@ require 'formula'
 class ChrubyMysql < Formula
   homepage 'https://github.com/dhallman/ruby-envs/wiki/chruby-mysql'
   head 'https://github.com/dhallman/ruby-envs.git', :revision => '51d7410'
+  version '0.0.3'
 
   depends_on 'chruby'
   depends_on 'ruby-build'
