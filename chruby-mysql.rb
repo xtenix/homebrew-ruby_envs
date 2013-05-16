8a30055f200b6c2253752094ed42397e7e199bad
Dean Hallman <dhallman@realgravity.com>
2013-05-15 21:14:49 -0400

    initial revision

--- chruby-mysql.rb
+++ chruby-mysql.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class ChrubyMysql < Formula
   homepage 'https://github.com/dhallman/ruby-envs/wiki/chruby-mysql'
-  head 'https://github.com/dhallman/ruby-envs.git'
+  head 'https://github.com/dhallman/ruby-envs.git', :revision => '51d7410'
 
   depends_on 'chruby'
   depends_on 'ruby-build'
