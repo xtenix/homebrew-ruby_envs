04d5b3758f67c5d3ea82c27251e138bc116d5e57
Dean Hallman <dhallman@realgravity.com>
2013-05-15 23:52:24 -0400

    typo fix

--- chruby-mysql.rb
+++ chruby-mysql.rb
@@ -11,7 +11,7 @@ class ChrubyMysql < Formula
   depends_on 'chruby'
   depends_on 'ruby-build'
   depends_on 'mysql'
-  depends_on 'phize/homebrew-cask/brew-cask'
+  depends_on 'phinze/homebrew-cask/brew-cask'
 
   def install
     puts "installed."
