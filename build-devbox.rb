67e4f728b4c0423aad47511b67787033f201d38b
Dean Hallman <dhallman@realgravity.com>
2013-05-17 04:35:28 -0400

    iterating

--- build-devbox.rb
+++ build-devbox.rb
@@ -13,18 +13,18 @@ class BuildDevbox < Formula
   #require_tap 'phinze/cask'
   repair_taps
   require_tap 'homebrew/dupes'
-  depends_on 'autoconf'
-  depends_on 'automake'
-  depends_on 'libtool'
-  depends_on 'pkg-config'
-  depends_on 'apple-gcc42'
-  depends_on 'libyaml'
-  depends_on 'readline'
-  depends_on 'libxml2'
-  depends_on 'libxslt'
-  depends_on 'libksba'
-  depends_on 'openssl'
-  depends_on 'sqlite'
+  #depends_on 'autoconf'
+  #depends_on 'automake'
+  #depends_on 'libtool'
+  #depends_on 'pkg-config'
+  #depends_on 'apple-gcc42'
+  #depends_on 'libyaml'
+  #depends_on 'readline'
+  #depends_on 'libxml2'
+  #depends_on 'libxslt'
+  #depends_on 'libksba'
+  #depends_on 'openssl'
+  #depends_on 'sqlite'
   depends_on 'chruby'
   depends_on 'ruby-build'
   depends_on 'bash-completion'
@@ -37,7 +37,7 @@ class BuildDevbox < Formula
 
   def finalize_ruby_build(fi)
     ohai "Installing ruby 1.9.3-p429..."
-    system "ruby-build 1.9.3-p429 ~/.rubies/"
+    system "ruby-build 1.9.3-p429 ~/.rubies/1.9.3-p429"
     system "gem install rubygems-update"
     system "gem update --system"
   end
