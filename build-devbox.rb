4d803873c041673edeac6c9642aedd9be39d967e
Dean Hallman <dhallman@realgravity.com>
2013-05-20 03:07:49 -0400

    added full set of libraries

--- build-devbox.rb
+++ build-devbox.rb
@@ -13,33 +13,32 @@ class BuildDevbox < Formula
   #require_tap 'phinze/cask'
   repair_taps
   require_tap 'homebrew/dupes'
-  #depends_on 'autoconf'
-  #depends_on 'automake'
-  #depends_on 'libtool'
-  #depends_on 'pkg-config'
-  #depends_on 'apple-gcc42'
-  #depends_on 'libyaml'
-  #depends_on 'readline'
-  #depends_on 'libxml2'
-  #depends_on 'libxslt'
-  #depends_on 'libksba'
-  #depends_on 'openssl'
-  #depends_on 'sqlite'
+  depends_on 'autoconf'
+  depends_on 'automake'
+  depends_on 'libtool'
+  depends_on 'pkg-config'
+  depends_on 'apple-gcc42'
+  depends_on 'libyaml'
+  depends_on 'readline'
+  depends_on 'libxml2'
+  depends_on 'libxslt'
+  depends_on 'libksba'
+  depends_on 'openssl'
+  depends_on 'sqlite'
   depends_on 'chruby'
   depends_on 'ruby-build'
   depends_on 'bash-completion'
-  #depends_on 'wget'
-  #depends_on 'curl'
-  #depends_on 'ack'
-  #depends_on 'vim'
-  #depends_on 'mysql'
+  depends_on 'wget'
+  depends_on 'curl'
+  depends_on 'ack'
+  depends_on 'vim'
+  depends_on 'mysql'
   #depends_on 'brew-cask'
 
   def finalize_ruby_build(fi)
     ohai "Installing ruby 1.9.3-p429..."
-    system "ruby-build 1.9.3-p429 ~/.rubies/1.9.3-p429"
-    system "gem install rubygems-update"
-    system "gem update --system"
+    #system "ruby-build 1.9.3-p429 ~/.rubies/1.9.3-p429"
+    #system "gem install bundler"
   end
   
   # Wrap up the whole install
